284ee2e79130b04526bb48744c60ad3d5fb2bf72
JamesKinler <james193722@yahoo.com>
2016-12-30 17:58:43 -0800

    i added some jquery and other things in the last few days to make my plugin have a better style

--- wp-content/themes/realestatechild/template-realestatelistings.php
+++ wp-content/themes/realestatechild/template-realestatelistings.php
@@ -8,7 +8,13 @@
 
 <?php if(dynamic_sidebar('front_page_widget')) : else : endif?>
 
-
+  <select class="chosen" multiple="true" style="width:200px;">
+  	<option>Choose...</option>
+  	<option>jQuery</option>
+  	<option selected="selected">MooTools</option>
+  	<option>Prototype</option>
+  	<option>Dojo Toolkit</option>
+  </select>
 
 
 
